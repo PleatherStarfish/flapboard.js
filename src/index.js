6b2da6cbc2a8abb46abf774550c687e6384c053e
Daniel Miller <lontano.composer@gmail.com>
2019-09-18 23:53:13 -0400

    Add to function

--- src/index.js
+++ src/index.js
@@ -1,38 +1,26 @@
-import "./styles.css";
-
-const element = document.getElementById("splitflap");
-
-const upNow = element.getAttribute("data-upnow");
-const upNext = element.getAttribute("data-upnext");
-
-let flipcard = document.createElement("div");
-flipcard.innerHTML = `<div class="scene scene--card">
-
-  <div class="background">
-    <div class="number__top">${upNext}</div>
-  </div>
-
-  <div class="card">
-
-    <div class="card__face card__face--front-top">
-      <div class="number__top">${upNow}</div>
-    </div>
-    
-    <div class="card__face card__face--back">
-      <div class="number__top--flipped">${upNext}</div>
-    </div>
-
-  </div>
-
-  <div class="card__face card__face--front-bottom">
-    <div class="number__bottom">${upNow}</div>
-  </div>
-
-  <div class="gap"></div>
-
-</div>`;
-
-element.appendChild(flipcard);
+const flipboard = new Flipboard({
+  parentNode: "splitflap",
+  numberOfFlipboards: 6,
+  flipTime: "0.2s",
+  moduloOrNextUp: "modulo",
+  initialString: "DANIEL",
+  synchronous: true,
+  clickable: false,
+  rollingOrStepThrough: "rolling",
+  rollingDelay: "0.5s",
+  updateDirection: "random",
+  fontSize: "200pt",
+  color: "white",
+  backgroundColor: "black",
+  gapWidth: "4px",
+  gapColor: "white",
+  height: "260px",
+  width: "200px",
+  margin: "40px 5px",
+  font: "sans-serif",
+  fontWeight: "bold",
+  perspective: "1000px"
+});
 
 const card = document.querySelector(".card");
 card.addEventListener("click", () => {
